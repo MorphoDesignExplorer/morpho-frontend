6b2d58cae9dc87d09b0e979200969bdb2148e221
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2024-07-23 13:46:35 +0530

    migrated to ts + small design cleanups

--- src/routes/[project_name]/XYGraph.svelte
+++ src/routes/[project_name]/XYGraph.svelte
@@ -1,40 +1,47 @@
-<script>
+<script lang="ts">
     import * as d3 from "d3";
 
-    /** @type {number} */
-    let width;
-    /** @type {number} */
-    let height;
-    /** @type {string[]}*/
-    export let parameters;
-    /** @type {{id: number | string, parameters: Object<string, string|number>, output_parameters: Object<string, string|number>, files: Object<string, string>[]}[]} */
-    export let models;
-    /** @type {{filter: boolean, grid: boolean, graph: boolean, sidepane: boolean}}*/
-    export let display_options;
-    
+    interface Model {
+        id: number | string,
+        scoped_id: number | string,
+        parameters: Record<string, string|number>,
+        output_parameters: Record<string, string|number>,
+        files: Record<string, string>[],
+    }
+
+    interface Tooltip {
+        tooltip_div: HTMLDivElement,
+        tooltip_id: string | number,
+        tooltip_x: string | number,
+        tooltip_y: string | number
+    }
+
+    let width: number;
+    let height: number;
+    export let parameters: string[];
+    export let models: Model[];
+    export let display_options: {filter: boolean, grid: boolean, graph: boolean, sidepane: boolean};
+
     export let set_project;
 
-    /** @type {SVGElement}*/
-    let render_target;
-    /** @type {{tooltip_div: HTMLDivElement, tooltip_id: string | number, tooltip_x: string | number, tooltip_y: string | number}}*/
-    let tooltip = {
-        tooltip_div: null,
-        tooltip_id: null,
-        tooltip_x: null,
-        tooltip_y: null
+    let render_target: SVGElement;
+    let tooltip: Tooltip = {
+        tooltip_id: "",
+        tooltip_x: "",
+        tooltip_y: ""
     };
 
     let x_paraname = "";
     let y_paraname = "";
 
-    // data is a set of items of form [x_parameter_value, y_parameter_value, id]
-    /** @type {[string | number, string | number, string | number][]}*/
-    let data = [];
+    // data is a set of items of form [x_parameter_value, y_parameter_value, id, scoped_id]
+    let data: (string|number)[][] = [];
     $: if (x_paraname != "" && y_paraname != "") {
         data = models.map(model => [
             (x_paraname in model.parameters)? model.parameters[x_paraname] : model.output_parameters[x_paraname],
             (y_paraname in model.parameters)? model.parameters[y_paraname] : model.output_parameters[y_paraname],
-            model.id
+            model.id,
+            model.scoped_id
         ])
     }
 
@@ -43,9 +50,13 @@
     export let marginBottom = 30;
     export let marginLeft = 4*20;
 
-    let gx;
-    let gy;
-    let point_set;
+
+    let canvas: SVGElement;
+    let gx: SVGElement;
+    let gy: SVGElement;
+    let point_set: SVGElement;
+    let g_xlabel: SVGTextElement;
+    let g_ylabel: SVGTextElement;
     let zoom;
 
     function zoomed({transform}) {
@@ -67,21 +78,22 @@
     $: d3.select(gx).call(d3.axisBottom(x));
     $: d3.select(render_target).call(zoom).call(zoom.transform, d3.zoomIdentity);
 
-    /** @type {string} */
     let grid_position = "grid-column: 1 / 2; grid-row: 1 / 3;";
+
+
 </script>
 
-<div class="h-full border-r-2 border-r-amber-950 flex flex-col items-center relative" style={grid_position}>
+<div class="h-full border-r-2 border-r-gray-200 pb-4 flex flex-col items-center relative" style={grid_position}>
     <button
         on:click={() => {
             display_options.graph = !display_options.graph;
         }}
-        class="absolute top-1 left-1 w-fit p-1 bg-amber-950 text-orange-200 hover:text-amber-950 hover:bg-orange-200 transition ease-in-out font-bold z-10">
+        class="absolute top-4 left-4 w-fit p-1 bg-blue-600 text-orange-200 hover:text-blue-600 hover:bg-orange-200 transition ease-in-out font-bold z-10 rounded-md shadow-md">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
     </button>
-    <div class="w-full h-5/6 flex flex-col items-center" bind:clientHeight={height} bind:clientWidth={width} bind:this={render_target}>
+    <div class="w-full h-full flex flex-col items-center" bind:clientHeight={height} bind:clientWidth={width} bind:this={render_target}>
         <svg class="w-full h-full select-none">
             <g bind:this={gx} transform="translate(0,{height - marginBottom})" />
             <g bind:this={gy} transform="translate({marginLeft},0)" />
@@ -100,7 +112,7 @@
                             tooltip["tooltip_div"].style.display = "block";
                             tooltip["tooltip_x"] = datum[0];
                             tooltip["tooltip_y"] = datum[1];
-                            tooltip["tooltip_id"] = datum[2];
+                            tooltip["tooltip_id"] = datum[3];
                         }}
                         on:mouseout={event => {
                             tooltip["tooltip_div"].style.opacity = "0%";
@@ -113,17 +125,17 @@
         </svg>
         <div
             bind:this={tooltip["tooltip_div"]}
-            class="absolute bg-black font-mono text-white p-2 bg-opacity-55 w-fit flex-col transition ease-in-out opacity-0 hidden"
+            class="absolute bg-black text-white p-2 bg-opacity-55 w-fit flex-col transition ease-in-out opacity-0 hidden"
         >
             <p>id: {tooltip.tooltip_id}</p>
             <p>{x_paraname}: {tooltip.tooltip_x}</p>
             <p>{y_paraname}: {tooltip.tooltip_y}</p>
         </div>
     </div>
-    <div class="flex flex-col w-4/5 gap-2 font-mono">
+    <div class="flex w-4/5 gap-2 text-xs">
         <div class="flex flex-row w-full items-center gap-2">
             <p class="font-bold">X:</p>
-            <select name="x-select" bind:value={x_paraname} class="border-2 border-amber-950 p-2 w-full">
+            <select name="x-select" bind:value={x_paraname} class="border border-gray-200 bg-white p-2 px-3 rounded-md w-full capitalize">
                 <option value="" disabled selected>x parameter</option>
                 {#each parameters as parameter}
                 <option value={parameter}>{parameter}</option>
@@ -132,7 +144,7 @@
         </div>
         <div class="flex flex-row w-full items-center gap-2">
             <p class="font-bold">Y:</p>
-            <select name="x-select" bind:value={y_paraname} class="border-2 border-amber-950 p-2 w-full">
+            <select name="x-select" bind:value={y_paraname} class="border-[1px] border-gray-200 bg-white p-2 px-3 rounded-md w-full capitalize">
                 <option value="" disabled selected>y parameter</option>
                 {#each parameters as parameter}
                 <option value={parameter}>{parameter}</option>
